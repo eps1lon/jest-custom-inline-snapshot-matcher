6af6e21ac8c35132fd1e3b274e00b532ee8a431b
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-09-27 18:22:49 +0200

    Add async matcher

--- index.test.js
+++ index.test.js
@@ -1,11 +1,23 @@
 const { toMatchInlineSnapshot } = require("jest-snapshot");
 const { toMatchCaseInsensitiveInlineSnapshot } = require("./matchers");
 
+function sleep(timeoutMs) {
+  return Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeoutMs);
+  });
+}
+
 expect.extend({
   toMatchTrimmedInlineSnapshot(received) {
     return toMatchInlineSnapshot.call(this, received.substring(0, 10));
   },
   toMatchCaseInsensitiveInlineSnapshot,
+  async toMatchAsyncInlineSnapshot(received) {
+    await sleep(1000);
+    return toMatchInlineSnapshot.call(this, received);
+  },
 });
 
 test("custom internal toMatchInlineSnapshot", () => {
@@ -18,6 +30,10 @@ test("custom external toMatchInlineSnapshot", () => {
   expect(
     "Some text with uppercase letters."
   ).toMatchCaseInsensitiveInlineSnapshot(`"Some text with uppercase letters."`);
+});
+
+test("custom async toMatchInlineSnapshot", async () => {
+  await expect("text").toMatchCaseInsensitiveInlineSnapshot(`"text"`);
 });
 
 test("toMatchInlineSnapshot", () => {
